3f478898d5a051851d2cc023d62bcb7835545134
thushi308 <thushi3082006@gmail.com>
2024-12-29 02:43:57 +0530

    text added

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,19 +1,72 @@
-const swiper = new Swiper('.swiper', {
+
+const swiper = new Swiper('.swiper-competitions', {
   // Optional parameters
   grabCursor: true,
   spaceBetween: 34,
+  rewind: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    PauseOnMouseEnter: true,
+  },
+  centerInsufficientSlides: true,
 
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination-competitions',
     clickable: true,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next-competitions',
+    prevEl: '.swiper-button-prev-competitions',
+    breakpoints : {
+      0: {
+        hiddenClass: true
+      },
+      730: {
+        hiddenClass: false
+      },
+    }
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    730: {
+      slidesPerView: 2
+    },
+    1055: {
+      slidesPerView: 3
+    },
+  }
+});
+
+const swiper_judges = new Swiper('.swiper-judges', {
+  // Optional parameters
+  grabCursor: true,
+  spaceBetween: 34,
+  rewind: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    PauseOnMouseEnter: true,
+  },
+  centerInsufficientSlides: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-judges',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next-judges',
+    prevEl: '.swiper-button-prev-judges',
     breakpoints : {
       0: {
         hiddenClass: true
